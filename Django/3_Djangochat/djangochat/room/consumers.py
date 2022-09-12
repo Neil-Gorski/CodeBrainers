b917918b6ae1c4565d403edd8519328eea968f7a
Neil Gorski <neil.gorski@hotmail.ch>
2022-09-12 15:45:08 +0200

    init neil

---
+++ Django/3_Djangochat/djangochat/room/consumers.py
@@ -0,0 +1,44 @@
+import json
+from re import A, S
+from django.contrib.auth.models import User
+from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import sync_to_async
+from .models import Room, Message
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = "chat_%s" % self.room_name
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        
+        await self.accept()
+
+    async def disconnect(self):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+    
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        print(data)
+        message = data['message']
+        username = data['username']
+        room = data['room']
+
+        await self.save_message(username, room, message)
+
+
+    # Send message to room group
+        await self.channel_layer.group_send(self.room_group_name,
+        {
+        'type': 'chat_message',
+        'message': message,
+        'username': username
+        }
+        )
